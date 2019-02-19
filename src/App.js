9537432d735f79d698462c41df7af50a62377efa
Denytheman <danjo83@gmail.com>
2019-02-18 17:54:49 -0800

    Added some css to style person and button

--- src/App.js
+++ src/App.js
@@ -34,11 +34,21 @@ class App extends Component {
   }
 
   render() {
+    const style ={
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi</h1>
         <p>This is some text</p>
-        <button onClick={() => this.switchNameHandler('Maxwel')}>Switch name</button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Maxwel')}>Switch name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}/>
