584f5ee8ad2cc1bf42f048ecf6f94bb7306d927b
Daniel Kovac <dkovac@logitech.com>
2019-02-19 11:30:12 -0800

    Added Radium package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium'
 import Person from './Person/Person'
 
 class App extends Component {
@@ -50,7 +51,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -70,6 +75,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+          backgroundColor: 'salmon',
+          color: 'white'
+      }
     }
 
     let classes = [];
@@ -94,4 +103,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
