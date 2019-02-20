2561d9ca4b096d15e922f84d52885cd138cab038
Daniel Kovac <dkovac@logitech.com>
2019-02-20 11:25:35 -0800

    PropTypes to define input parameters for components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, {Component, Fragment} from 'react';
+import PropTypes from 'prop-types';
 import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 
@@ -9,10 +10,20 @@ class Person extends Component {
       <Fragment>
           <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
           <p>{this.props.children}</p>
-          <input type="text" onChange={this.props.changed} value={this.props.name}/>
+          <input 
+            type="text" 
+            onChange={this.props.changed} 
+            value={this.props.name}/>
       </Fragment>
     )
   };
+};
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
 };
 
 export default withClass(Person, classes.Person);
