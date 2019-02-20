32178ec9a3265782eee34589eb650f5bf4157e43
Daniel Kovac <dkovac@logitech.com>
2019-02-20 11:30:38 -0800

    Setting up a reference to an html object

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,6 +4,10 @@ import withClass from '../../../hoc/withClass';
 import classes from './Person.css';
 
 class Person extends Component {  
+  componentDidMount() {
+    this.inputElement.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering...')
     return (
@@ -11,6 +15,7 @@ class Person extends Component {
           <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
           <p>{this.props.children}</p>
           <input 
+            ref={(inputEl) => { this.inputElement = inputEl }}
             type="text" 
             onChange={this.props.changed} 
             value={this.props.name}/>
